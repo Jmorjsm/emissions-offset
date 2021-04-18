5942c17425bdb2e8f72e353b3f60ae2af98fe775
Jon Morgan <jmorjsm@gmail.com>
2021-04-18 10:02:32 +0100

    starting implementation of list view for trip history

--- src/emissions_offset/lib/main.dart
+++ src/emissions_offset/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:emissions_offset/data/trip_recorder.dart';
 import 'package:geolocator/geolocator.dart';
 
+import 'models/trip.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -75,6 +77,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     getLocation();
+
+    var allTrips;
+    final trips = List<Trip>.generate(3, (i) => allTrips[i]);
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -87,41 +93,13 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              'Position:',
-            ),
-            Text(
-                '$_position',
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: trips.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text('${trips[index].endTime}'),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
